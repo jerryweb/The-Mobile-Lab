ab59e4bb099715703d74f7a09bd385c7eaf57c4a
Jerry Webb <ljwebbdev8800@gmail.com>
2021-05-23 16:24:39 -0700

    Created the UIKit Version of the Mobile Lab Project
    Created the initial UIKit Version of the Mobile Lab Project

---
+++ UIKit/The Mobile Lab with UIKit/The Mobile Lab with UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Mobile Lab with UIKit
+//
+//  Created by Jerry on 5/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
