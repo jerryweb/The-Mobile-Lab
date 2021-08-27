eaac71571168ff758e780b282aed0727d37a12d0
Jerry Webb <ljwebbdev8800@gmail.com>
2021-08-26 22:51:59 -0700

    Added the mobile lab UI project for test the file playback of the sound generator framework.

--- MobileLab/MobileLabTests/MobileLabTests.swift
+++ MobileLab/MobileLabTests/MobileLabTests.swift
@@ -9,20 +9,5 @@ import XCTest
 @testable import MobileLab
 
 class MobileLabTests: XCTestCase {
-    let soundGenerator = SoundGeneratorModel(generatorName: nil)
-    
-    func test_CreateNewSoundGenerator_withGivenName() throws {
-        let kickSoundGenerator = SoundGeneratorModel(generatorName: "Kick")
-
-        XCTAssertEqual(kickSoundGenerator.name, "Kick")
-    }
-    
-    
-    func test_CreateNewSoundGenerator_verifyDefaultValues() throws {
-                
-        XCTAssertEqual(soundGenerator.name, "Sample")
-        XCTAssertEqual(soundGenerator.volume, 1.0)
-        XCTAssertEqual(soundGenerator.pan, 0.0)
-        XCTAssertEqual(soundGenerator.audioFile, nil)
-    }
+  
 }
