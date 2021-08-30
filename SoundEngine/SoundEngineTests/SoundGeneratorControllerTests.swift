7f2e527bc006d646e9408bb0c87ded03dd2fd81a
Jerry Webb <ljwebbdev8800@gmail.com>
2021-08-29 17:58:37 -0700

    Refactored SoundGeneratorControllerTests setup data.  Added volume limit testing.

--- SoundEngine/SoundEngineTests/SoundGeneratorControllerTests.swift
+++ SoundEngine/SoundEngineTests/SoundGeneratorControllerTests.swift
@@ -11,20 +11,21 @@ import XCTest
 // MARK: Sound Generator Controller Tests
 class SoundGeneratorControllerTests : XCTestCase {
     
+    // MARK: Properties
     let soundGeneratorController = SoundGeneratorController()
+    let soundGeneratorModel = SoundGeneratorModel(generatorName: "Sound Generator Model")
+
+    
+    // MARK: Tests
+    override func setUp() {
+        createSUT()
+    }
     
     func test_SetSoundGeneratorModelForSoundGeneratorController() throws {
-        
-        let soundGeneratorModel = SoundGeneratorModel(generatorName: "Sound Generator Model")
-        soundGeneratorController.setSoundGenerator(soundGeneratorModel)
-        
         XCTAssertNotNil(soundGeneratorController.getSoundGenerator())
     }
     
     func test_ModfiySoundGeneratorVolume() throws {
-        
-        let soundGeneratorModel = SoundGeneratorModel(generatorName: "Sound Generator Model")
-        soundGeneratorController.setSoundGenerator(soundGeneratorModel)
         
         XCTAssertEqual(soundGeneratorController.getSoundGenerator()?.volume , 1.0)
         soundGeneratorController.setVolume(0.333)
@@ -33,24 +34,21 @@ class SoundGeneratorControllerTests : XCTestCase {
     
     func test_ModfiySoundGeneratorPan() throws {
         
-        let soundGeneratorModel = SoundGeneratorModel(generatorName: "Sound Generator Model")
-        soundGeneratorController.setSoundGenerator(soundGeneratorModel)
-        
         XCTAssertEqual(soundGeneratorController.getSoundGenerator()?.pan , 0.0)
         soundGeneratorController.setPan(-0.259)
         XCTAssertEqual(soundGeneratorController.getSoundGenerator()?.pan , -0.259)
     }
     
 
-//    func test_VerifySoundGeneratorVolumeLimits() throws {
-//        XCTAssertEqual(soundGenerator.volume, 1.0)
-//
-//        soundGenerator.setVolume(1.00001)
-//        XCTAssertEqual(soundGenerator.volume, 1.0)
-//
-//        soundGenerator.setVolume(-7.0)
-//        XCTAssertEqual(soundGenerator.volume, 0.0)
-//    }
+    func test_VerifySoundGeneratorVolumeLimits() throws {
+        XCTAssertEqual(soundGeneratorController.getSoundGenerator()?.volume, 1.0)
+
+        soundGeneratorController.setVolume(1.00001)
+        XCTAssertEqual(soundGeneratorController.getSoundGenerator()?.volume, 1.0)
+
+        soundGeneratorController.setVolume(-7.0)
+        XCTAssertEqual(soundGeneratorController.getSoundGenerator()?.volume, 0.0)
+    }
 //
 //    func test_VerifySoundGeneratorPanLimits() throws {
 //        XCTAssertEqual(soundGenerator.pan, 0.0)
@@ -69,5 +67,9 @@ class SoundGeneratorControllerTests : XCTestCase {
 //    }
 
     
+// MARK: Helpers
+    func createSUT(){
+        soundGeneratorController.setSoundGenerator(soundGeneratorModel)
+    }
 }
 
