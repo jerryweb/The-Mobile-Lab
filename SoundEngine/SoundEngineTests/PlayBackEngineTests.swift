25137cbc341c954688ff6db87f9bc4927216ea83
Jerry Webb <ljwebbdev8800@gmail.com>
2021-10-20 22:43:22 -0700

    Added Delete mixer track test for playback engine. May need to reconsider how tracks are stored (in an array) since the track instance can be removed from the system. This will cause a possible desync in the name of the track ("track 1") and physical index of the track in the mixer tracks array. This may not matter in actual implementation as the track names should contain the audio file getting played by the track.

--- SoundEngine/SoundEngineTests/PlayBackEngineTests.swift
+++ SoundEngine/SoundEngineTests/PlayBackEngineTests.swift
@@ -24,6 +24,13 @@ class PlaybackEngine {
         audioEngine.connect(mixerTrack.audioMixerNode, to: audioEngine.mainMixerNode, format: mixerTrack.audioMixerNode.outputFormat(forBus: 0))
     }
     
+    func deleteMixerTrack(trackNumber: Int){
+        if trackNumber >= 0 && trackNumber < mixerTracks.count {
+            audioEngine.detach(mixerTracks[trackNumber].audioMixerNode)
+            mixerTracks.remove(at: trackNumber)
+        }
+    }
+    
     func startEngine() {
         audioEngine.prepare()
         do {
@@ -49,33 +56,34 @@ class MixerTrack {
         self.name = name
         audioMixerNode = AVAudioMixerNode()
     }
+    
+    deinit {
+        print("Mixer track \(name) has been deinitialized.")
+    }
 }
 
 class PlaybackEngineTests: XCTestCase {
     // MARK: Properties
     let playbackEngine = PlaybackEngine()
+    var engine = AVAudioEngine()
     
     // MARK: Tests
     override func setUpWithError() throws {
-        
+        engine = playbackEngine.audioEngine
     }
 
     override func tearDownWithError() throws {
         
     }
     
-    func test_CreatePlaybackEngine(){
-        let engine = playbackEngine.audioEngine
-        
+    func test_createPlaybackEngine(){
         XCTAssertFalse(engine.isRunning)
         XCTAssertEqual(engine.attachedNodes.count, 0)
         XCTAssertEqual(engine.mainMixerNode.nextAvailableInputBus, 0)
         XCTAssertTrue(playbackEngine.mixerTracks.isEmpty)
     }
     
-    func test_CreateMixerTrack(){
-        let engine = playbackEngine.audioEngine
-        
+    func test_createMixerTrack(){
         XCTAssertTrue(playbackEngine.mixerTracks.isEmpty)
 
         playbackEngine.createMixerTrack()
@@ -86,8 +94,6 @@ class PlaybackEngineTests: XCTestCase {
     }
     
     func test_createMultipleMixerTracks(){
-        let engine = playbackEngine.audioEngine
-        
         XCTAssertTrue(playbackEngine.mixerTracks.isEmpty)
 
         for _ in 0...9{
@@ -102,9 +108,22 @@ class PlaybackEngineTests: XCTestCase {
         XCTAssertEqual(engine.mainMixerNode.nextAvailableInputBus, 10)
     }
     
-    func test_StartAudioEngine(){
-        let engine = playbackEngine.audioEngine
+    func test_deleteMixerTrack(){
+        playbackEngine.createMixerTrack()
+        playbackEngine.createMixerTrack()
         
+        XCTAssertEqual(playbackEngine.mixerTracks.count, 2)
+        XCTAssertEqual(engine.attachedNodes.count, 4)
+        XCTAssertEqual(engine.mainMixerNode.nextAvailableInputBus, 2)
+        
+        playbackEngine.deleteMixerTrack(trackNumber: 0)
+        
+        XCTAssertEqual(playbackEngine.mixerTracks.count, 1)
+        XCTAssertEqual(engine.attachedNodes.count, 3)
+        XCTAssertEqual(engine.mainMixerNode.nextAvailableInputBus, 0)
+    }
+    
+    func test_startAudioEngine(){
         playbackEngine.createMixerTrack()
         XCTAssertFalse(engine.isRunning)
         
@@ -112,8 +131,7 @@ class PlaybackEngineTests: XCTestCase {
         XCTAssertTrue(engine.isRunning)
     }
     
-    func test_StopAudioEngine(){
-        let engine = playbackEngine.audioEngine
+    func test_stopAudioEngine(){
         playbackEngine.createMixerTrack()
         playbackEngine.startEngine()
         
