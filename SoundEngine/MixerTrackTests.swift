f83b7284b468752719fc5f69eb661972b76c98b6
Jerry Webb <ljwebbdev8800@gmail.com>
2021-10-23 15:43:04 -0700

    Reduced MixerTrackTests class

--- SoundEngine/MixerTrackTests.swift
+++ SoundEngine/MixerTrackTests.swift
@@ -9,6 +9,7 @@ import XCTest
 @testable import SoundEngine
 
 class MixerTrackTests: XCTestCase {
+    let mixerTrack = MixerTrack(name: "Track 1")
     
     override func setUpWithError() throws {
         
@@ -20,8 +21,6 @@ class MixerTrackTests: XCTestCase {
     
     // MARK: Tests
     func test_createMixerTrack(){
-        let mixerTrack = MixerTrack(name: "Track 1")
-        
         XCTAssertFalse(mixerTrack.muted)
         XCTAssertNotNil(mixerTrack.audioMixerNode)
         XCTAssertEqual(mixerTrack.name, "Track 1")
@@ -29,8 +28,6 @@ class MixerTrackTests: XCTestCase {
     }
     
     func test_muteAndUnmuteMixerTrack(){
-        let mixerTrack = MixerTrack(name: "Track 1")
-        
         mixerTrack.mute()
         XCTAssertTrue(mixerTrack.muted)
         
@@ -39,7 +36,6 @@ class MixerTrackTests: XCTestCase {
     }
     
     func test_changeVolume(){
-        let mixerTrack = MixerTrack(name: "Track 1")
         XCTAssertEqual(mixerTrack.audioMixerNode.volume, 0.5)
         
         mixerTrack.changeVolume(0.33346)
@@ -47,7 +43,6 @@ class MixerTrackTests: XCTestCase {
     }
     
     func test_changePan(){
-        let mixerTrack = MixerTrack(name: "Track 1")
         XCTAssertEqual(mixerTrack.audioMixerNode.pan, 0.0)
         
         mixerTrack.changePan(-0.333)
@@ -55,7 +50,6 @@ class MixerTrackTests: XCTestCase {
     }
     
     func test_verifyVolumeLimits(){
-        let mixerTrack = MixerTrack(name: "Track 1")
         mixerTrack.changeVolume(-0.05)
         XCTAssertEqual(mixerTrack.audioMixerNode.volume, 0.0)
         
@@ -64,7 +58,6 @@ class MixerTrackTests: XCTestCase {
     }
     
     func test_verifyPanLimits(){
-        let mixerTrack = MixerTrack(name: "Track 1")
         mixerTrack.changePan(-1.333)
         XCTAssertEqual(mixerTrack.audioMixerNode.pan, -1.0)
         
